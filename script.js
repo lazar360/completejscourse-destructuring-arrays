b950ec6618d085520d61fb2831c1bab5d425f382
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-05-27 18:06:19 +0200

    maj script

--- script.js
+++ script.js
@@ -197,3 +197,40 @@ console.log(staffUnique);
 console.log(staffUnique.size);
 
 console.log(new Set('nicolasgautier').size);
+
+////////////////////////////////////////////////////////////////
+// Map
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenz, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('staffUnique', staffUnique)
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.siize);
+//rest.clear();
+// ne pas faire :
+//rest.set([1,2], 'Test')
+//console.log(rest.get([1,2])); // retournera undefined car pas le même objet
+// il faut créer un objet
+const arr = [1, 2];
+// et l'intégrer en clé
+rest.set([1, 2], Test);
+
+
